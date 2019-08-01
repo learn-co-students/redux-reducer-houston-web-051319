714a927776048df1ccd67be798ac21ca111e076e
Ahawkfizz <001.aqh@gmail.com>
2019-08-01 08:56:54 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+    if(action.type==="ADD_FRIEND"){
+        return {...state, friends: [...state.friends, action.friend]}
+    }
+    else if(action.type==="REMOVE_FRIEND"){
+        let removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+        return {...state, friends: [...state.friends.slice(0, removalIndex), 
+            ...state.friends.slice(removalIndex + 1)]}
+    }
+    return state;
 }
+
+  
